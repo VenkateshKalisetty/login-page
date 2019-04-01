8015b86aab29c7d70a1e5db13d61ae165e30530d
VenkateshKalisetty <venky12034@gmail.com>
2019-04-01 22:06:20 +0530

    UI Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'login-page';
+export class AppComponent implements OnInit {
+  avatar: string = "open";
+  passwordHide: boolean = true;
+  email: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  password: FormControl = new FormControl('', [Validators.required]);
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  getEmailErrorMessage() {
+    return this.email.hasError('required') ? '*Email required' :
+        this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  getPasswordErrorMessage() {
+    return this.password.hasError('required') ? '*Password required' : '';
+  }
+  onChangeAvatar() {
+    this.avatar = (this.password.value.length > 0) ? "close" : "open";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule, MatFormFieldModule, MatButtonModule, MatIconModule, MatInputModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
